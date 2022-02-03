d8d748767d93305ab99a28916b697f4d79b76a1b
Kyle Coffey <mariorules24@gmail.com>
2022-02-03 14:35:09 -0600

    Basic prompt.

--- bin/mc.ml
+++ bin/mc.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello World."
+let rec repl () =
+  print_string "> ";
+  match try Some (read_line ()) with End_of_file -> None with
+  | None -> print_endline ""
+  | Some line ->
+      (match line with
+      | "1 + 2 * 3" -> print_endline "7"
+      | _ -> print_endline "ERROR: Invalid expression!");
+      repl ()
+
+let () = repl ()
