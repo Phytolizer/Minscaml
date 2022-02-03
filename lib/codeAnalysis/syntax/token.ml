1ac30424220976492a85f7ed3a70b1b9a59044d7
Kyle Coffey <mariorules24@gmail.com>
2022-02-03 16:42:33 -0600

    Print token values when they are present.

--- lib/codeAnalysis/syntax/token.ml
+++ lib/codeAnalysis/syntax/token.ml
@@ -1,4 +1,10 @@
 type t = { kind : Kind.t; position : int; value : Object.t; text : string }
 
 let create kind position value text = { kind; position; value; text }
-let print t = Printf.sprintf "%s: '%s'" (Kind.print t.kind) t.text
+
+let print t =
+  Printf.sprintf "%s: '%s'" (Kind.print t.kind) t.text
+  ^
+  match t.value with
+  | Object.Null -> ""
+  | Object.Number n -> Printf.sprintf " %d" n
