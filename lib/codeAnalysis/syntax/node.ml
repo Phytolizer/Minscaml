cf981f7bba72935fcbaf1691a9799c4175076987
Kyle Coffey <me@phytolizer.dev>
2022-02-08 22:21:36 -0600

    Closer to compiling...

--- lib/codeAnalysis/syntax/node.ml
+++ lib/codeAnalysis/syntax/node.ml
@@ -1,12 +1,47 @@
-type t = Expression of ExpressionSyntax.t | Token of Token.t
+type t = Expression of expression_t | Token of Token.t
+
+and expression_t =
+  | Binary of {
+      left : expression_t;
+      operator_token : Token.t;
+      right : expression_t;
+    }
+  | Unary of { operator_token : Token.t; operand : expression_t }
+  | Literal of { literal_token : Token.t }
+  | Parenthesized of {
+      open_parenthesis_token : Token.t;
+      expression : expression_t;
+      close_parenthesis_token : Token.t;
+    }
+
+let print_expr e =
+  match e with
+  | Binary _ -> "BinaryExpressionSyntax"
+  | Unary _ -> "UnaryExpressionSyntax"
+  | Literal _ -> "LiteralExpressionSyntax"
+  | Parenthesized _ -> "ParenthesizedExpressionSyntax"
+
+let expr_children e =
+  match e with
+  | Binary { left; operator_token; right } ->
+      [ Expression left; Token operator_token; Expression right ]
+  | Unary { operator_token; operand } ->
+      [ Token operator_token; Expression operand ]
+  | Literal { literal_token } -> [ Token literal_token ]
+  | Parenthesized
+      { open_parenthesis_token; expression; close_parenthesis_token } ->
+      [
+        Token open_parenthesis_token;
+        Expression expression;
+        Token close_parenthesis_token;
+      ]
 
 let print n =
   match n with
-  | Expression e -> ExpressionSyntax.print e
+  | Expression e -> print_expr e
   | Token t -> print_string (Token.print t)
 
-let children n =
-  match n with Expression e -> ExpressionSyntax.children e | Token t -> []
+let children n = match n with Expression e -> expr_children e | Token t -> []
 
 let pretty_print n =
   let rec pretty_print_node n indent is_last =
